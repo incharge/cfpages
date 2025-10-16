7833c4d4b6d6dd8ea47bed10576fec07313cda7f
Julian Price <github.com@julianprice.org.uk>
2025-10-16 09:43:02 +0100

    Send an email

--- functions/api/submit.js
+++ functions/api/submit.js
@@ -1,7 +1,31 @@
+import { EmailMessage } from "cloudflare:email";
+import { createMimeMessage } from "mimetext";
+
 /**
  * POST /api/submit
  */
 export async function onRequestPost(context) {
+    try {
+        const msg = createMimeMessage();
+        msg.setSender({ name: "GPT-4", addr: "sender@knownas.co.uk" });
+        msg.setRecipient("cloudflare@incharge.co.uk");
+        msg.setSubject("An email generated in a worker");
+        msg.addMessage({
+            contentType: "text/plain",
+            data: `Congratulations, you just sent an email from a worker.`,
+        });
+
+        var message = new EmailMessage(
+            "sender@knownas.co.uk",
+            "cloudflare@incharge.co.uk",
+            msg.asRaw(),
+        );
+
+        await context.env.SEB.send(message);
+    } catch (e) {
+      return new Response(e.message);
+    }
+
   try {
     let input = await context.request.formData();
     let pretty = JSON.stringify([...input], null, 2);
